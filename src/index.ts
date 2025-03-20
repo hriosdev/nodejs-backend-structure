8f443758d20fa4e84521488f8deaf390980a27c1
hriosdev <hrios.dev@proton.me>
2025-03-20 12:56:17 -0700

    Node Js Structure with Typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const port = process.env.PORT || 5000;
+const environment = process.env.ENVIRONMENT || "dev";
+const app = express();
+
+// Middleware configuration
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Run the server
+app.listen(port, () => {
+  console.log(`Server running in '${environment}' at http://localhost:${port}`);
+});
